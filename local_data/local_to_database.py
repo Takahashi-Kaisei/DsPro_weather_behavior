3e60a7a5183a3e1ae47f300ad2db9f503da2b2bd
Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
2024-01-14 13:38:51 +0900

    ipynbファイルで検証、問題なく動くことがわかった

--- local_data/local_to_database.py
+++ local_data/local_to_database.py
@@ -10,10 +10,10 @@ class Local2Database:
 
     def convert(self, local_csv):
         #私のlocalファイルにアクセスしています
-        df = pd.read_csv(f'{local_csv}')
+        df = pd.read_csv(local_csv)
 
 
         #変換しかしないので注意
-        con = sqlite3.connect(f'{local_csv}') #データベースにアクセス
+        con = sqlite3.connect('/Users/takahashikaisei/Lecture/ds_programing/final_kadai/local_data/screantime_local.sqlite') #データベースにアクセス
         df.to_sql('screantime_data', con, if_exists='replace', index=False) #データベースに変換、if_exists='replace'で同じ名前のものがあったときに置き換える。index=Falseでindexを入れないように指定してる
         con.close() #データベースを閉じる
